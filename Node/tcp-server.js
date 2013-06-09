9a7336b702655d4be921b87f7d5d92d8637323e5
Dustin Moorman <dustin.moorman@gmail.com>
2013-06-08 23:21:42 -0500

    added echo to socket

--- Node/tcp-server.js
+++ Node/tcp-server.js
@@ -5,6 +5,10 @@ var net = require('net');
 var server = net.createServer(function(socket){
 	socket.write('howdy\n');
 	socket.end('world!\n');
+
+	socket.on('data', function(data) {
+		socket.write(data);
+	})
 });
 
 server.listen(1337);
