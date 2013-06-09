0d27aeadda42840599bdc1095a593d3193c75187
Dustin Moorman <dustin.moorman@gmail.com>
2013-06-08 23:16:07 -0500

    Added telnet testing results

--- Node/tcp-server.js
+++ Node/tcp-server.js
@@ -8,3 +8,24 @@ var server = net.createServer(function(socket){
 });
 
 server.listen(1337);
+
+//Notes and tests! - - - -
+
+
+// -- Telnet
+
+//Command:
+//telnet localhost 1337
+
+//Response:
+//Trying ::1...
+//telnet: connect to address ::1: Connection refused
+//Trying 127.0.0.1...
+//Connected to localhost.
+//Escape character is '^]'.
+//howdy
+//world!
+//Connection closed by foreign host.
+
+
+
