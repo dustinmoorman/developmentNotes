6fd85b4e193d41e60e405c607e8229f3be37d06e
Dustin Moorman <dustin.moorman@gmail.com>
2013-06-08 23:31:40 -0500

    added chat server

---
+++ Node/chat-server.js
@@ -0,0 +1,19 @@
+//a simple sauce chat server made of node!
+
+net = require('net');
+
+var sockets = [];
+
+var server = net.Server(function(socket){
+	socket.write("::ChatServer:: ~ it is made of node");
+	sockets.push(socket);
+	socket.on('data', function(data){
+		for (var i = 0; i < sockets.length; i++) {
+			sockets[i].write(data);
+		}
+	});
+});
+
+server.listen(1337);
+
+
