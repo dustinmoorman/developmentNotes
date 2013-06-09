ebd29893a8d329e3a717b334d756cc0844f877a2
Dustin Moorman <dustin.moorman@gmail.com>
2013-06-08 22:20:28 -0500

    Added note from curling the hello world

--- Node/node-hello-world.js
+++ Node/node-hello-world.js
@@ -7,3 +7,11 @@ http.createServer( function(request, response) {
 	response.end('Hello World\n');
 }).listen(1337, '127.0.0.1');
 
+
+//curling!
+//curl -i http://localhost:1337
+//HTTP/1.1 200 OK
+//Content-Type: text/plain
+//Date: Sun, 09 Jun 2013 03:13:50 GMT
+//Connection: keep-alive
+//Transfer-Encoding: chunked
