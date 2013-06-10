ec0f087c43d767ce09c96c29400480f853818ac8
Dustin Moorman <dustin.moorman@gmail.com>
2013-06-09 23:03:24 -0500

    made it better

--- Node/interval-server.js
+++ Node/interval-server.js
@@ -1,5 +1,6 @@
 //Multi process example
 
+//Say hello every 5 seconds
 setInterval(function() {
 	console.log("hello world!");
 }, 5000);
@@ -8,12 +9,15 @@ console.log("Interval time!");
 
 var http = require('http');
 
+//Fetch google response headers every 2 s
 setInterval( function() {
 	console.log("fetching google.com");
 	
 	http.get({ host: 'google.com' }, function (response) {
 		console.log(response.headers);
 	});
+
+	console.log("fetch complete");
 }, 2000);
 
 console.log("do your thing!");
