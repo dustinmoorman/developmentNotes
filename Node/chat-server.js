7e5fd25592a3fa8f43465422791ae4c8e4f854ec
Dustin Moorman <dustin.moorman@gmail.com>
2013-06-09 22:28:22 -0500

    spacing for clarity

--- Node/chat-server.js
+++ Node/chat-server.js
@@ -5,8 +5,10 @@ net = require('net');
 var sockets = [];
 
 var server = net.Server(function(socket){
+
 	socket.write("::ChatServer:: ~ it is made of node\n");
 	sockets.push(socket);
+
 	socket.on('data', function(data){
 		for (var i = 0; i < sockets.length; i++) {
 			sockets[i].write(data);
